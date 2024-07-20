ba9ff482598e99f4f6f9312adbc6a3c80bf2d59b
Ramazan Özer <96844411+RamazanOzerr@users.noreply.github.com>
2024-07-20 22:00:38 +0300

    Add files via upload

---
+++ data/who_we_are.js
@@ -0,0 +1,10 @@
+const whoWeAreText = `
+Her markanın benzersiz bir hikayesi olduğuna inanıyor ve bu hikayeleri hayata geçirmek için çalışıyoruz. 
+Müşterilerimizin vizyonlarını gerçekleştirmeleri için gerekli stratejileri ve çözümleri sunuyoruz. 
+Markaların dijital dünyada güçlü bir varlık göstermesi, hedef kitleleriyle anlamlı bağlar kurması ve sürdürülebilir bir başarı elde etmesi için çalışıyoruz. 
+Siz de markanızın hikayesini en iyi şekilde anlatmak ve dijital dünyada kalıcı izler bırakmak istiyorsanız, Pia ile tanışın. 
+Hayallerinizin peşinden gitmeye hazırız.
+`;
+const whoWe = 'idk';
+
+
