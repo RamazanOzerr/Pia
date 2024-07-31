fe6173142d68ea5a8b7b04a7f76bf02c5c2f0d2c
NurettinKaradag <nurettinkaradag45@gmail.com>
2024-08-01 01:42:25 +0300

    update index.html and css

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     cards.forEach(card => {
         card.addEventListener('click', () => {
+            cards.forEach(c => {
+                if (c !== card) {
+                    c.classList.remove('flipped');
+                }
+            });
             card.classList.toggle('flipped');
         });
     });
